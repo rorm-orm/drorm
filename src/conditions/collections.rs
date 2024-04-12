128022b982005821495b792d934237d09d943e8d
gammelalf <gammelalf@hopfen.space>
2024-04-12 12:09:37 +0200

    Implemented condition collections for `Option<impl Condition>`

--- src/conditions/collections.rs
+++ src/conditions/collections.rs
@@ -2,11 +2,53 @@
 //!
 //! This module provides two flavours:
 //! - [a dynamic one](DynamicCollection)
-//! - a static one
+//! - [a static one](StaticCollection)
 //!
 //! Where static and dynamic mean whether the collection's size is known at compile time.
+//!
+//! A static collection is best created using the [`and!`](macro@crate::and)
+//! and [`or!`](macro@crate::or) macros which use the same syntax as `vec!`.
+//! They will automatically work around `StaticCollection`'s size restriction
+//! by resorting to a `DynamicCollection` of boxes if needed.
+//!
+//! Both collection types can either be use with conditions or `Option` of conditions.
+//! This second mode is useful when you're dealing with a lot of `Condition`s which are
+//! only added conditionally. This makes a static collection effectively into a dynamic one with
+//! a capacity known at compile time:
+//!
+//! ```rust
+//! # use rorm::{and, query};
+//! # use rorm_db::Database;
+//! # use rorm::prelude::*;
+//! # #[derive(Model)]
+//! # struct User {
+//! #     #[rorm(id)]
+//! #     id: i64,
+//! #     #[rorm(max_length = 255)]
+//! #     name: String,
+//! #     region: String,
+//! # }
+//! async fn query_users(
+//!     db: &Database,
+//!     filter_name: Option<String>,   // Optional filters
+//!     filter_region: Option<String>, // applied to query
+//! ) -> Vec<User> {
+//!     // The `and`'s capacity is always 2
+//!     // but the actual number of conditions are only known at runtime.
+//!     // Also note the "AND" of an empty set is "TRUE"
+//!     // yielding the expected result of not filtering the users at all.
+//!     query!(db, User)
+//!         .condition(and![
+//!             filter_name.map(|name| User::F.name.equals(name)),
+//!             filter_region.map(|region| User::F.region.equals(region)),
+//!         ])
+//!         .all()
+//!         .await
+//!         .unwrap()
+//! }
+//! ```
 
-use rorm_db::sql::conditional;
+use rorm_db::sql::{conditional, value};
 
 use super::Condition;
 use crate::internal::query_context::QueryContext;
@@ -22,6 +64,8 @@ pub enum CollectionOperator {
 
 /// A collection of conditions with a dynamic size.
 ///
+/// (See [module](self) for some general information)
+///
 /// The generic type `T` is the conditions' type, which has to be a single one.
 /// (See [Disadvantage](DynamicCollection#sisadvantage))
 ///
@@ -31,7 +75,7 @@ pub enum CollectionOperator {
 /// ## Disadvantage:
 /// - All conditions have to be of the same type.
 ///     This can be mitigated by erasing their type using [Condition::boxed].
-///     In this case use [BoxedCondition<'a>](super::BoxedCondition) for the generic variable `T`.
+///     In this case use [`BoxedCondition<'a>`](super::BoxedCondition) for the generic variable `T`.
 #[derive(Clone)]
 pub struct DynamicCollection<T> {
     /// Operator used for joining, i.e. `and` or `or`
@@ -40,9 +84,12 @@ pub struct DynamicCollection<T> {
     /// Vector of conditions
     pub vector: Vec<T>,
 }
-impl<A> DynamicCollection<A> {
+impl<'a, T> DynamicCollection<T>
+where
+    Self: Condition<'a>,
+{
     /// Create a vector of conditions joined by AND
-    pub fn and(vector: Vec<A>) -> Self {
+    pub fn and(vector: Vec<T>) -> Self {
         Self {
             operator: CollectionOperator::And,
             vector,
@@ -50,7 +97,7 @@ impl<A> DynamicCollection<A> {
     }
 
     /// Create a vector of conditions joined by OR
-    pub fn or(vector: Vec<A>) -> Self {
+    pub fn or(vector: Vec<T>) -> Self {
         Self {
             operator: CollectionOperator::Or,
             vector,
@@ -58,7 +105,7 @@ impl<A> DynamicCollection<A> {
     }
 }
 
-impl<'a, A: Condition<'a>> Condition<'a> for DynamicCollection<A> {
+impl<'a, T: Condition<'a>> Condition<'a> for DynamicCollection<T> {
     fn add_to_context(&self, context: &mut QueryContext) {
         for cond in self.vector.iter() {
             cond.add_to_context(context);
@@ -77,11 +124,40 @@ impl<'a, A: Condition<'a>> Condition<'a> for DynamicCollection<A> {
         )
     }
 }
+impl<'a, T: Condition<'a>> Condition<'a> for DynamicCollection<Option<T>> {
+    fn add_to_context(&self, context: &mut QueryContext) {
+        for cond in self.vector.iter().flatten() {
+            cond.add_to_context(context);
+        }
+    }
+
+    fn as_sql(&self, context: &QueryContext) -> conditional::Condition {
+        let conditions: Vec<_> = self
+            .vector
+            .iter()
+            .flatten()
+            .map(|condition| condition.as_sql(context))
+            .collect();
+        if conditions.is_empty() {
+            conditional::Condition::Value(value::Value::Bool(match self.operator {
+                CollectionOperator::And => true,
+                CollectionOperator::Or => false,
+            }))
+        } else {
+            (match self.operator {
+                CollectionOperator::And => conditional::Condition::Conjunction,
+                CollectionOperator::Or => conditional::Condition::Disjunction,
+            })(conditions)
+        }
+    }
+}
 
 /// A collection of conditions with static size.
 ///
+/// (See [module](self) for some general information)
+///
 /// The generic parameter `T` is a tuple of conditions.
-/// Only tuple with 8 elements or less are allowed.
+/// Only tuple with 8 elements or fewer are allowed.
 /// (See [Disadvantage](StaticCollection#disadvantage))
 ///
 /// ## Advantage
@@ -89,7 +165,7 @@ impl<'a, A: Condition<'a>> Condition<'a> for DynamicCollection<A> {
 ///
 /// ## Disadvantage
 /// - Due to rust's limitations, there has to be a maximum number of elements this tuple can hold.
-///     Currently it is set to 8, which is an arbitrary choice, but there has to be one.
+///     Currently, it is set to 8, which is an arbitrary choice, but there has to be one.
 #[derive(Copy, Clone)]
 pub struct StaticCollection<T> {
     /// Operator used for joining, i.e. `and` or `or`
@@ -98,7 +174,10 @@ pub struct StaticCollection<T> {
     /// Tuple of conditions
     pub tuple: T,
 }
-impl<T> StaticCollection<T> {
+impl<'a, T> StaticCollection<T>
+where
+    Self: Condition<'a>,
+{
     /// Create a tuple of conditions joined by AND
     pub fn and(tuple: T) -> Self {
         Self {
@@ -143,6 +222,35 @@ macro_rules! impl_static_collection {
                 ])
             }
         }
+
+        #[allow(non_snake_case)] // the macro is simpler when generic variable are reused as value variables
+        impl<'a, $($generic: Condition<'a>),+> Condition<'a> for StaticCollection<($(Option<$generic>,)+)> {
+            fn add_to_context(&self, context: &mut QueryContext) {
+                let ($($generic,)+) = &self.tuple;
+                $(if let Some(cond) = $generic {
+                    cond.add_to_context(context);
+                })+
+            }
+
+            fn as_sql(&self, context: &QueryContext) -> conditional::Condition {
+                let ($($generic,)+) = &self.tuple;
+                let mut conditions = Vec::new();
+                $(if let Some(cond) = $generic {
+                    conditions.push(cond.as_sql(context));
+                })+
+                if conditions.is_empty() {
+                    conditional::Condition::Value(value::Value::Bool(match self.operator {
+                        CollectionOperator::And => true,
+                        CollectionOperator::Or => false,
+                    }))
+                } else {
+                    (match self.operator {
+                        CollectionOperator::And => conditional::Condition::Conjunction,
+                        CollectionOperator::Or => conditional::Condition::Disjunction,
+                    })(conditions)
+                }
+            }
+        }
     };
     ($($generic:ident),+) => {
         impl_static_collection!(recu $($generic),+);
@@ -151,7 +259,7 @@ macro_rules! impl_static_collection {
 // Current max tuple size is 8
 impl_static_collection!(H, G, F, E, D, C, B, A);
 
-/// A common definition for the or! and and! macro
+/// A common definition for the [`or!`] and [`and!`] macro
 #[doc(hidden)]
 #[macro_export]
 macro_rules! create_collection {

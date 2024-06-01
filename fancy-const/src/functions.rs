224ca34518686cbb8d957f715eaf00ef788986b8
gammelalf <gammelalf@hopfen.space>
2024-06-01 14:25:15 +0200

    Documented fancy-const

--- fancy-const/src/functions.rs
+++ fancy-const/src/functions.rs
@@ -1,19 +1,174 @@
-//! Provide the two central traits:
-//! - [`Contains`] which defines values i.e. arguments
+//! `const fn` in traits
+//!
+//! Traits can have 3 kinds of associated objects: `fn`, `const` and `type`.
+//! `fn` can't be const yet, so we have to abuse one of the other two to build a workaround.
+//! `const` could be used, but their type and therefore their possible implementations have
+//! to be defined by the trait author. (See [example](#Alternative) below)
+//! This module uses the third option (associated types).
+//!
+//! It provides two central traits:
+//! - [`Contains`] which defines values i.e. arguments and returns
 //! - [`ConstFn`] which defines functions
+//!
+//! # Basic Idea
+//!
+//! The basic idea is to have some type `add` (lower case because it should represent a function)
+//! which implements `ConstFn<(i32, i32), i32>`.
+//! The trait `ConstFn<(i32, i32), i32>` contains one associated constant
+//! which takes a `(i32, i32)` as argument and is of type `i32`:
+//! ```compile_fail
+//! pub trait IdealConstFn<Args, Ret> {
+//!     const RETURN<const ARGS: Args>: Ret;
+//! }
+//!
+//! pub struct add;
+//! impl IdealConstFn<(i32, i32), i32> for add {
+//!     const RETURN<const ARGS: (i32, i32))>: i32 = {
+//!         let (x, y) = ARGS;
+//!
+//!         // The actual function body
+//!         x + 1
+//!     };
+//! }
+//!
+//! const THREE: i32 = <add as IdealConstFn<(i32, i32), i32>>::RETURN::<(1, 2)>;
+//! ```
+//!
+//! # Actual implementation
+//!
+//! However, this ideal has two problems:
+//! 1. associated consts can't be generic
+//! 2. const generics can't be tuples
+//!
+//! Solving 1. is not much of an issue, just introduce another generic as indirection:
+//! ```compile_fail
+//! pub trait NotSoIdealConstFn<Args, Ret> {
+//!     type Call<const ARGS: Args>: Return<Ret>;
+//! }
+//! pub trait Return<Ret> {
+//!     const RETURN: Ret;
+//! }
+//!
+//! pub struct add;
+//! impl NotSoIdealConstFn<(i32, i32), i32> for add {
+//!     type Call<const ARGS: (i32, i32))> = addBody<ARGS>;
+//! }
+//! pub struct addBody<const: ARGS: (i32, i32)>;
+//! impl<const ARGS: (i32, i32)> Return<i32> for addBody<ARGS> {
+//!     const RETURN: i32 = {
+//!         let (x, y) = ARGS;
+//!
+//!         // The actual function body
+//!         x + 1
+//!     };
+//! }
+//! ```
+//!
+//! Solving 2. is doable but makes calling `ConstFn`s really unergonomic.
+//! Since we can't have const generics, we have to fall back to normal generics.
+//! However, this means, we have to have one type for each value we might want to pass to a function:
+//! ```
+//! pub trait Contains<T> {
+//!     const ITEM: T;
+//! }
+//! pub struct OneAndOne;
+//! impl Contains<(i32, i32)> for OneAndOne {
+//!     const ITEM: (i32, i32) = (1, 1);
+//! }
+//! ```
+//!
+//! Actually `Contains<T>` and `Returns<Ret>` look the same, so we can just use `Contains` which brings us to the actual implementation:
+//! ```
+//! pub trait Contains<T> {
+//!     const ITEM: T;
+//! }
+//! pub trait ConstFn<Arg, Ret> {
+//!     // Named it `Body` instead of `Call`
+//!     // which makes more sense from the implementors perspective.
+//!     type Body<T: Contains<Arg>>: Contains<Ret>;
+//! }
+//!
+//! pub struct add;
+//! impl ConstFn<(i32, i32), i32> for add {
+//!     type Body<T: Contains<(i32, i32)>> = addBody<T>;
+//! }
+//! pub struct addBody<T>([T; 0]);
+//! impl<T: Contains<(i32, i32)>> Contains<i32> for addBody<T> {
+//!     const ITEM: i32 = {
+//!         let (x, y) = T::ITEM;
+//!
+//!         // actual body
+//!         x + y
+//!     };
+//! }
+//!
+//! pub struct OneAndTwo;
+//! impl Contains<(i32, i32)> for OneAndTwo {
+//!     const ITEM: (i32, i32) = (1, 2);
+//! }
+//! const THREE: i32 = <<add as ConstFn<(i32, i32), i32>>::Body::<OneAndTwo> as Contains<i32>>::ITEM;
+//! ```
+//!
+//! # Alternative
+//! As described in the introduction paragraph, you could use associated `const`s.
+//!
+//! Their huge advantage is readability:
+//!     There is no type magic, just basic rust.
+//!
+//! However, they also have a huge disadvantage:
+//!     The trait author has to decide on the fixed set of possible methods an implementor may choose from.
+//!
+//! ```rust
+//! trait SomeTrait {
+//!     const SOME_METHOD: SomeMethod;
+//! }
+//! enum SomeMethod {
+//!     ImplemenationA,
+//!     ImplemenationB,
+//!     // ...
+//! }
+//! impl SomeMethod {
+//!     const fn call(self) {
+//!         match self {
+//!             Self::ImplemenationA => {},
+//!             Self::ImplemenationB => {},
+//!             // ...
+//!         }
+//!     }
+//! }
+//! ```
 
+/// Attaches a constant value of type `T`.
+///
+/// See [module docs](self) for more information about how and why.
 pub trait Contains<T> {
+    /// The value attached to / represented by `Self`
     const ITEM: T;
 }
 
+/// A `const fn(...Arg) -> Ret` which can be used in traits.
+///
+/// See [`const_fn!`](crate::const_fn) for an easy way to implement this.
+///
+/// See [module docs](self) for more information about how and why.
 pub trait ConstFn<Arg, Ret> {
+    /// A type which is generic over `T` and uses `T::ITEM` in its `Contains` implementation
+    /// to compute the "return" value.
     type Body<T: Contains<Arg>>: Contains<Ret>;
 }
 
 /// Converts a normal function into a [`ConstFn`].
 ///
-/// - Due to limitations with `tt` the body requires double braces!
-/// - Only accepts a very simple `fn` syntax!
+/// Only accepts a very simple `fn` syntax!
+///
+/// ```
+/// # use fancy_const::const_fn;
+/// const_fn! {
+///     fn add(x: i32, y: i32) -> i32 {
+///         x + y
+///     }
+/// }
+/// ```
 #[macro_export]
 macro_rules! const_fn {
     (#[doc = $doc:literal] $vis:vis fn $fun_name:ident($( $arg_name:ident : $arg_type:ty ),+) -> $ret_type:ty $body:block) => {
@@ -37,6 +192,8 @@ macro_rules! const_fn {
 mod wrappers {
     macro_rules! impl_wrappers {
         ($( $wrapper:ident: $typ:ty ),+$(,)?) => {$(
+            /// Type which provides a simple implementation for
+            #[doc = concat!("[`Contains<", stringify!($typ), ">.`]")]
             pub struct $wrapper<const ITEM: $typ>;
             impl<const ITEM: $typ> $crate::Contains<$typ> for $wrapper<ITEM> {
                 const ITEM: $typ = ITEM;
--- fancy-const/src/lib.rs
+++ fancy-const/src/lib.rs
@@ -1,3 +1,7 @@
+//! Various utilities, concepts and hacks to improve `const`
+
+#![warn(missing_docs)]
+
 mod functions;
 
 pub use functions::*;
